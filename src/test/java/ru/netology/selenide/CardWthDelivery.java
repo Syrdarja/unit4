df1844ed1368524fe3fe5692fe28ae636e6b52f4
MariMax <maxim.prohachev@yandex.ru>
2024-06-13 19:50:30 +0300

    add test city

--- src/test/java/ru/netology/selenide/CardWthDelivery.java
+++ src/test/java/ru/netology/selenide/CardWthDelivery.java
@@ -1,5 +1,6 @@
 package ru.netology.selenide;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -29,4 +30,19 @@ public class CardWthDelivery {
         $("[data-test-id='notification']").shouldBe(visible,Duration.ofSeconds(15));
         $("[data-test-id='notification']").shouldHave(text("Встреча успешно забронирована на " + planningDate));
     }
+    @Test
+    public void findCityBySeveralLettersTest() {
+        open ("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Яр");
+        $$(".menu-item__control").findBy(text("Ярославль")).click();
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Орлова Ольга");
+        $("[data-test-id='phone'] input").setValue("+70001122333");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id='notification']").shouldBe(visible,Duration.ofSeconds(15));
+        $("[data-test-id='notification']").shouldHave(text("Встреча успешно забронирована на " + planningDate));
+    }
+
     }
